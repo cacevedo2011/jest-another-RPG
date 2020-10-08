ec07f16b1c4f85e5d6abea372e4f330c6d37dec5
cacevedo2011 <cristianacevedo2011@gmail.com>
2020-10-08 16:43:32 -0400

    added some coding

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -20,6 +20,7 @@ test('creates a player object', () => {
   });
 
 test("gets player's stats as an object", () => {
+
     const player = new Player('Dave');
   
     expect(player.getStats()).toHaveProperty('potions');
@@ -48,4 +49,36 @@ test('gets inventory from player or returns false', () => {
     player.inventory = [];
   
     expect(player.getInventory()).toEqual(false);
+  });
+
+test("gets player's health value", () => {
+
+    const player = new Player('Dave');
+  
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+  });
+
+test('checks if player is alive or not', () => {
+
+    const player = new Player('Dave');
+  
+    expect(player.isAlive()).toBeTruthy();
+  
+    player.health = 0;
+  
+    expect(player.isAlive()).toBeFalsy();
+  });
+
+test("subtracts from player's health", () => {
+  
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+  
+    player.reduceHealth(5);
+  
+    expect(player.health).toBe(oldHealth - 5);
+  
+    player.reduceHealth(99999);
+  
+    expect(player.health).toBe(0);
   });
